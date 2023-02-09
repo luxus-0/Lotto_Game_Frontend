4a559f62e724a36d32dfa7184f2eb0d64d9edd76
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2023-02-09 19:15:18 +0100

    adding inputs

--- src/pages/PlayPage.tsx
+++ src/pages/PlayPage.tsx
@@ -5,6 +5,20 @@ import { Button, FormContainer, Header, Input } from './PlayPage.styles';
 export const PlayPage = () => {
   const [inputs, setInputs] = useState(Array(6).fill(''));
 
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number,
+  ) => {
+    const updatedInputs = [...inputs];
+    updatedInputs[index] = event.target.value;
+    setInputs(updatedInputs);
+  };
+
+  const fillRandom = () => {
+    const randomInputs = Array.from({ length: 6 }, () => Math.floor(Math.random() * 100));
+    setInputs(randomInputs.map(String));
+  };
+
   return (
     <>
       <Header>
@@ -12,11 +26,22 @@ export const PlayPage = () => {
         <div>Enter your lucky numbers or just fill the fields randomly</div>
       </Header>
       <FormContainer>
-        {inputs.map((input, index) => (
-          <Input key={index} type="number" value={input} />
-        ))}
-        <Button type="button">Random</Button>
-        <Button type="submit">Submit</Button>
+        <div>
+          {inputs.map((input, index) => (
+            <Input
+              key={index}
+              type="number"
+              value={input}
+              onChange={(event) => handleInputChange(event, index)}
+            />
+          ))}
+        </div>
+        <div>
+          <Button type="button" onClick={fillRandom}>
+            Random
+          </Button>
+          <Button type="submit">Submit</Button>
+        </div>
       </FormContainer>
     </>
   );
