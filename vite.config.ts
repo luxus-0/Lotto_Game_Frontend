27dc677055224dc46323cf3e4c7702d09e460562
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-04-11 12:16:25 +0200

    vite rollabacked

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://ec2-3-65-14-143.eu-central-1.compute.amazonaws.com:8000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
